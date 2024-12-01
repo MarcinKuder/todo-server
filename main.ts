3e8b11721af0b2da4956f1cc92db3eabb4877806
Marcin Kuder <marcin.kuder@gmail.com>
2024-12-01 13:46:26 +0100

    fixes2

--- main.ts
+++ main.ts
@@ -12,61 +12,54 @@ interface Task {
   updatedAt?: string;
 }
 
-app.post("/task", async (c) => {
+app.post("/tasks", async (c) => {
   try {
     let { name, priority } = await c.req.json();
-    if (
-      typeof name !== "string" ||
-      (priority != null && typeof priority !== "number")
-    ) {
-      throw new Error("Invalid request data");
+    if (typeof name !== "string") {
+      throw new Error("Name is required");
     }
-    if (priority == null) {
-      priority = 1;
-    }
-    const id = crypto.randomUUID();
     const task: Task = {
-      id,
+      id: crypto.randomUUID(),
       name,
       done: false,
-      priority,
+      priority: priority || 1,
       createdAt: new Date().toISOString(),
     };
-    await kv.set(["task", id], task);
+    await kv.set(["tasks", id], task);
     return c.json({
       message: `Task "${name}" has been added to the list!`,
     });
   } catch (e: any) {
-    return c.json({ message: `Invalid request data. ${e.message}` }, 400);
+    return c.json({ message: `Error: ${e.message}` }, 400);
   }
 });
 
-app.get("/task", async (c) => {
-  const task: Deno.KvEntry<Task>[] = await Array.fromAsync(
-    kv.list({ prefix: ["task"] }),
+app.get("/tasks", async (c) => {
+  const tasks: Deno.KvEntry<Task>[] = await Array.fromAsync(
+    kv.list({ prefix: ["tasks"] }),
   );
   const hideDone = c.req.query("hideDone") === "true";
   return c.json(
-    task
+    tasks
       .filter((task) => !hideDone || !task.value.done)
       .map((task) => task.value),
   );
 });
 
-app.get("/task/:id", async (c) => {
+app.get("/tasks/:id", async (c) => {
   const id = c.req.param("id");
-  const task = await kv.get(["task", id]);
+  const task = await kv.get(["tasks", id]);
   if (task.value === null) {
     return c.json({ message: "Task not found" }, 404);
   }
   return c.json(task.value);
 });
 
-app.delete("/task/:id", async (c) => {
+app.delete("/tasks/:id", async (c) => {
   const id = c.req.param("id");
-  const task = await kv.get(["task", id]);
+  const task = await kv.get(["tasks", id]);
   if (task.value !== null) {
-    await kv.delete(["task", id]);
+    await kv.delete(["tasks", id]);
     return c.json({
       message: `Task has been deleted!`,
     });
@@ -74,30 +67,37 @@ app.delete("/task/:id", async (c) => {
   return c.json({ message: "Task not found" }, 404);
 });
 
-app.delete("/deleteAll", async (c) => {
-  const tasks: Deno.KvListIterator<Task> = kv.list({ prefix: ["task"] });
+app.delete("/tasks", async (c) => {
+  const tasks: Deno.KvListIterator<Task> = kv.list({ prefix: ["tasks"] });
+  const authorized = c.req.query("key") === "1111";
+  if (!authorized) {
+    return c.json({ message: "Unauthorized" }, 401);
+  }
   for await (const task of tasks) {
-    await kv.delete(["task", task.value.id]);
+    await kv.delete(["tasks", task.value.id]);
   }
   return c.json({ message: "All tasks have been deleted!" });
 });
 
-app.put("/task/:id", async (c) => {
+app.put("/tasks/:id", async (c) => {
   try {
     const id = c.req.param("id");
     const { name, done, priority } = await c.req.json();
-    let updatedTask = (await kv.get(["task", id])).value as Task;
+    let updatedTask = (await kv.get(["tasks", id])).value as Task;
+    if (updatedTask === null) {
+        return c.json({ message: "Task not found" }, 404);
+    }
     if (name !== undefined) {
-        updatedTask.name = name;
+      updatedTask.name = name;
     }
     if (done !== undefined) {
-        updatedTask.done = done;
+      updatedTask.done = done;
     }
     if (priority !== undefined) {
-        updatedTask.priority = priority;
+      updatedTask.priority = priority;
     }
     updatedTask.updatedAt = new Date().toISOString();
-    await kv.set(["task", id], updatedTask);
+    await kv.set(["tasks", id], updatedTask);
     return c.json({ message: `Task updated!` });
   } catch (e: any) {
     return c.json({ message: `Error: ${e.message}` }, 400);
